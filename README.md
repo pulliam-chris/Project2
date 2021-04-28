d128a5101452e83fee6833eb950ee484e74a97bb
PatriceW <tolmie2@gmail.com>
2021-04-27 19:57:30 -0700

    created highcharts js

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Project2
+# Project2 - US Auto Accident Analysis
+
+Members:
+Chris Pulliam
+Brienne Cole
+Patrice Woida
+Rylee Ahnen
+
+Kaggle has a recent dataset of gathered US Auto Accidents.  The accident contain Lat Lng location information so we plan to clean the data and make visualization clustering the accident markers to analyze high volume areas.
+
+Then we plan to collect GEO JSON data on those high volume accident areas to make another visualization representing the start-end lat lngs for the accidents and representing them in neightborhood areas such as the Portland neighborhood information listed below.
+
+We also plan the factor in the severity of the accidents aligned with the neighborhoods or separately based on our analysis and/or using the time stamp information as a heatmap or figure.
+
+With those data sets considering adding in demographic information or calls for service from local police agency.
+
+US Accident Dataset (2016-2020, 4.2 million records) - initially we will likely parse down the dataset to cover 2019-2020 due to the volume of records.
+https://www.kaggle.com/sobhanmoosavi/us-accidents
+(Start Time
+End Time
+Start Lat Lng
+End Lat Lng
+Severity)
+
+
+Possible Geo JSON (neighborhood files)
+https://gis-pdx.opendata.arcgis.com/datasets/neighborhood-boundaries?geometry=-123.189%2C45.459%2C-122.146%2C45.627
--- app.py
+++ app.py
@@ -1,19 +1,53 @@
 from flask import Flask, render_template, redirect, url_for, jsonify
 from flask_pymongo import PyMongo
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+#import plot
+import pprint
+import numpy as np
 
 app = Flask(__name__)
 
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/accidents_db"
+mongo = PyMongo(app)
+
+# Or set inline
+# mongo = PyMongo(app, uri="mongodb://localhost:27017/accidents")
+
+pp = pprint.PrettyPrinter(indent=4)
+accidentinfo = mongo.db.accidents.find( {} )
+pp.pprint(accidentinfo)
 
 @app.route("/")
+def index():
+    dbinfo = mongo.db.accidents.find({}, {"_id": 0 })
+    accidentinfo = jsonify(tuple(dbinfo))
+    #pp.pprint(accidentinfo)
+    return render_template("index.html", accidentinfo = accidentinfo)
 
 
 
+@app.route("/api/v1.0/accidents")
+def accidents():
+    dbinfo = mongo.db.accidents.find({}, {"_id": 0 })
+    accidentinfo = jsonify(tuple(dbinfo))
+    #pp.pprint(accidentinfo)
+    return accidentinfo
+#    #return render_template("index.html", accidentinfo=accidentinfo)
+#    return redirect(url_for('index'), code=302)
 
+@app.route("/api/v1.0/census")
+def census():
+    dbinfo = mongo.db.census.find({}, {"_id": 0 })
+    censusinfo = jsonify(tuple(dbinfo))
+    #pp.pprint(accidentinfo)
+    return censusinfo
 
-#keep this at the bottom
+@app.route("/api/v1.0/census/<year>")
+def census_year(year):
+    dbinfo = mongo.db.census.find({}, {"_id": 0, "Year": year, "Household Income": 1, "Median Age": 1, "Per Capita Income": 1, "Population" : 1, "Poverty Count" : 1, "Poverty Rate": 1, "Zipcode": 1})
+    censusinfo = jsonify(tuple(dbinfo))
+    #pp.pprint(accidentinfo)
+    return censusinfo
+
 if __name__ == "__main__":
     app.run(debug=True)
